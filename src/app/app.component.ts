97fc4fb5fc664b0ab3b7dcb32c1f02053f4d24bc
pranab <bluesunoccan@gmail.com>
2017-01-10 13:43:30 +0600

    Calculator input validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
-import { TabsPage } from '../pages/tabs/tabs';
+import {CalculatorPage} from "../pages/calculator/calculator";
 
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = TabsPage;
+  rootPage = CalculatorPage;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
